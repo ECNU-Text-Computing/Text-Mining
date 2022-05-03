f8c4f26f309890b80a08a733b5132ba446711518
yfduanECNU <yfduan@infor.ecnu.edu.cn>
2022-05-03 15:59:35 +0800

    增加模型

--- sequence_labeling/main.py
+++ sequence_labeling/main.py
@@ -11,6 +11,8 @@ import argparse
 import datetime
 import json
 import os
+import sys
+from utils.logger import Logger
 
 from dl.base_model import BaseModel
 from dl.bilstm import BiLSTM
@@ -37,7 +39,7 @@ dl_model_dict = {
     'Self_Attn': Self_Attention,
     'Self_Attn_Multi_Head': Self_Attention_Multi_Head,
     'SeqToSeq': SeqToSeq,
-    'SeqToSeq_DotProduct_Attn': SeqToSeq_DotProductAttn,
+    'SeqToSeq_DotProductAttn': SeqToSeq_DotProductAttn,
     'Transformer': Transformer
 }
 
@@ -46,14 +48,12 @@ def main_dl(config):
     if config['n_folds'] == 0:
         model = dl_model_dict[model_name](**config)
         print(model)
-        # model.run_model(model, 'train')
-        # model.run_model(model, 'test')
         model.run_train(model)
         model.test()
     elif config['n_folds'] > 0:
         for n in range(config['n_folds']):
             model = dl_model_dict[model_name](**config)
-            model.run_train(model)
+            print(model)
     else:
         raise RuntimeError("There is no n_folds in config.")
 
@@ -62,20 +62,21 @@ if __name__ == '__main__':
     start_time = datetime.datetime.now()
     parser = argparse.ArgumentParser(description='Process some description.')
     parser.add_argument('--phase', default='test', help='the function name.')
-
     args = parser.parse_args()
     print(args.phase)
+
     data_name = args.phase.strip().split('.')[0]
     model_cate = args.phase.strip().split('.')[1]
     config_path = './config/{}/{}/{}.json'.format(data_name, model_cate, args.phase)
-    # args = 'cmed.dl.base_model.norm'
-    # config_path = './config/cmed/dl/cmed.dl.base_model.norm.json'
     if not os.path.exists(config_path):
         raise RuntimeError("There is no {} config.".format(args))
     config = json.load(open(config_path, 'r'))
-    # print('config: ', config)
 
     model_name = config['model_name']
+    # 重定向输出至文件和控制台
+    sys.stdout = Logger('./log/{}.log'.format(model_name), sys.stdout)
+    print('config: ', config)
+
     if model_name in dl_model_dict:
         main_dl(config)
     else:
