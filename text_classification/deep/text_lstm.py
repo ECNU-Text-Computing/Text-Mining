54bb1051c4ec255af495a055515582f54290f5f7
theMoontoSun <56641962+theMoontoSun@users.noreply.github.com>
2022-07-17 21:33:23 +0800

    Update text_lstm.py

--- text_classification/deep/text_lstm.py
+++ text_classification/deep/text_lstm.py
@@ -20,83 +20,79 @@ LSTM的输出
 ======
 """
 
-import os
-import sys
-import argparse
 import datetime
+import argparse
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import numpy as np
-from utils.metrics import cal_all
-from Deep.Base_Model import Base_Model
+from deep.base_model import BaseModel
 
-#LSTM、Bi-LSTM文本分类
-
-class LSTM(Base_Model):
+class TextLSTM(BaseModel):
     def __init__(self, vocab_size, embed_dim, hidden_dim, num_classes,
                  dropout_rate, learning_rate, num_epochs, batch_size,
                  criterion_name, optimizer_name, gpu, **kwargs):
-        super(LSTM, self).__init__(vocab_size, embed_dim, hidden_dim, num_classes,
-                 dropout_rate, learning_rate, num_epochs, batch_size,
-                 criterion_name, optimizer_name, gpu, **kwargs)
+        # 继承父类BaseModel的属性
+        super(TextLSTM, self).__init__(vocab_size, embed_dim, hidden_dim, num_classes,
+                                       dropout_rate, learning_rate, num_epochs, batch_size,
+                                       criterion_name, optimizer_name, gpu, **kwargs)
 
-        #self.num_layers = 2                             #测试文件时没法调用config文件，所以直接设置神经网络的曾是
-        self.num_layers = kwargs['num_layers']          #神经网络的层数，可以自己取值,在json里改
-        self.num_directions = 2                         #取值为1或2，1为单向LSTM，2为双向LSTM，按照需求更改
+        # LSTM堆叠的层数，默认为2层
+        self.num_layers = 2
+        if 'num_layers' in kwargs:
+            self.num_layers = kwargs['num_layers']
+        # LSTM的方向性，双向LSTM则取值2；单向LSTM则取1
+        self.num_directions = 2
+        if 'num_directions' in kwargs:
+            self.num_directions = kwargs['num_directions']
+        self.bidirection = True if self.num_directions == 2 else False
 
-        self.lstm = nn.LSTM(embed_dim, hidden_dim, self.num_layers, batch_first=True, dropout=dropout_rate, bidirectional=True)
-
-        self.fc1 = nn.Linear(embed_dim * self.num_directions, hidden_dim * self.num_directions)
+        # 设置LSTM模型的参数
+        self.lstm = nn.LSTM(embed_dim, hidden_dim, self.num_layers,
+                            batch_first=True, dropout=dropout_rate, bidirectional=self.bidirection)
+        # 设置输出层的参数
         self.fc_out = nn.Linear(hidden_dim * self.num_directions, num_classes)
-        #self.dropout = nn.Dropout(self.dropout_rate)
 
-
+    # 模型的前向传播
     def forward(self, x):
-        input = self.embedding(x)
-        output, _ = self.lstm(input)
-
-        out = self.dropout(output[:, -1, :])  # 最后时刻的hidden state
-        out = self.fc1(out)
-        out = F.relu(out)
-        out = self.fc_out(out)
-
+        # batch_x: [batch_size, seq_len]
+        embed = self.embedding(x)  # [batch_size, seq_len, embed_dim]
+        hidden, _ = self.lstm(embed)  # [batch_size, seq_len, hidden_dim * self.num_directions]
+        # -1表示取该维度从后往前的第一个，即只需要最后一个词的隐藏状态作为输出
+        hidden = self.dropout(hidden[:, -1, :])  # [batch_size, hidden_dim * self.num_directions]
+        out = self.fc_out(hidden)  # [batch_size, num_classes]
         return out
 
-
+# 程序入口
 if __name__ == '__main__':
     start_time = datetime.datetime.now()
-    parser = argparse.ArgumentParser(description='Process some description.')
-    parser.add_argument('--phase', default='test', help='the function name.')
-
+    parser = argparse.ArgumentParser(description="Process some description.")
+    parser.add_argument('--phase', default='test', help='the function name')
     args = parser.parse_args()
 
-    #python3 text_lstm.py --phase test
     if args.phase == 'test':
-        print('This is a test process.')
-        # For testing our model, we can set the hyper-parameters as follows.
-        vocab_size, embed_dim, hidden_dim, num_classes,  \
+        print("This is a test process.")
+
+        # 设置测试用例，验证模型是否能够运行
+        # 设置模型参数
+        vocab_size, embed_dim, hidden_dim, num_classes, \
         dropout_rate, learning_rate, num_epochs, batch_size, \
-        criterion_name, optimizer_name, gpu = 200, 64, 64, 2, 0.5, 0.0001, 3, 64, 'CrossEntropyLoss', 'Adam', 0
+        criterion_name, optimizer_name, gpu = 100, 64, 64, 2, 0.5, 0.001, 3, 32, 'CrossEntropyLoss', 'Adam', 0
+        # 创建类的实例
+        model = TextLSTM(vocab_size, embed_dim, hidden_dim, num_classes,
+                         dropout_rate, learning_rate, num_epochs, batch_size,
+                         criterion_name, optimizer_name, gpu)
+        # 传入简单数据，查看模型运行结果
+        input_data = torch.LongTensor([[1, 3, 5, 7, 9], [2, 4, 6, 8, 10], [1, 4, 2, 7, 5]])
+        output_data = model(input_data)
+        print("The output is: {}".format(output_data))
 
-        # new an objective.
-        model = LSTM(vocab_size, embed_dim, hidden_dim, num_classes,
-                               dropout_rate, learning_rate, num_epochs, batch_size,
-                               criterion_name, optimizer_name, gpu)
-        # a simple example of the input.
-        input = [[1, 2, 3, 4, 5], [2, 3, 4, 5, 6], [3, 4, 5, 6, 7]]
-        input = torch.LongTensor(input)  # input: [batch_size, seq_len] = [3, 5]
-
-        # the designed model can produce an output.
-        output = model(input)
-        print(output)
-        print('This is a test process.')
+        print("The test process is done.")
 
     else:
-        print("There is no {} function. Please check your command.".format(args.phase))
-    end_time = datetime.datetime.now()
-    print('{} takes {} seconds.'.format(args.phase, (end_time - start_time).seconds))
+        print("There is no {} function.".format(args.phase))
 
-    print('Done LSTM_Model!')
+    end_time = datetime.datetime.now()
+    print("{} takes {} seconds.".format(args.phase, (end_time-start_time).seconds))
+    print("Done Text_LSTM.")
+
 
 
