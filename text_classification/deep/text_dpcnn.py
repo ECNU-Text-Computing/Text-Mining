ee83072bf6ab04d4d2a88aac68d4abf224419c7d
unknown <selena_gu@outlook.com>
2022-08-21 21:40:43 +0800

    Update text-dpcnn model

--- text_classification/deep/text_dpcnn.py
+++ text_classification/deep/text_dpcnn.py
@@ -19,10 +19,15 @@ class DPCNN(BaseModel):
         self.num_filters = 4
         if 'num_filters' in kwargs:
             self.num_filters = kwargs['num_filters']
+        # 卷积核大小（一次处理多少个词）
+        self.kernel_size = 3
+        if 'kernel_size' in kwargs:
+            self.kernel_size = kwargs['kernel_size']
+
         # 卷积层和池化层设定
-        self.conv_region = nn.Conv2d(1, self.num_filters, (3, self.embed_dim), stride=1)
-        self.conv = nn.Conv2d(self.num_filters, self.num_filters, (3, 1), stride=1)
-        self.max_pool = nn.MaxPool2d(kernel_size=(3, 1), stride=2)
+        self.conv_region = nn.Conv2d(1, self.num_filters, (self.kernel_size, self.embed_dim), stride=1)
+        self.conv = nn.Conv2d(self.num_filters, self.num_filters, (self.kernel_size, 1), stride=1)
+        self.max_pool = nn.MaxPool2d(kernel_size=(self.kernel_size, 1), stride=2)
         self.relu = nn.ReLU()
         # 零填充函数（左，右，上，下）
         self.padding1 = nn.ZeroPad2d((0, 0, 1, 1))  # 卷积前后的尺寸不变
@@ -30,23 +35,22 @@ class DPCNN(BaseModel):
         self.dropout = nn.Dropout(dropout_rate)
         self.fc = nn.Linear(self.num_filters, self.num_classes)
 
-    def forward(self, content):
-        # 不等长段落进行张量转化
-        x = self.embedding(content)  # [batch_size, seq_len, embed_dim]
+    def forward(self, x):
+        x = self.embedding(x)  # [batch_size, seq_len, embed_dim]
         x = x.unsqueeze(1)  # [batch_size, 1, seq_len, embed_dim]
-        x = self.conv_region(x)  # [batch_size, num_filters, seq_len-3+1, 1]
+        x = self.conv_region(x)  # [batch_size, num_filters, seq_len - kernel_size +1, 1]
 
         # padding保证等长卷积，先通过激活函数再卷积
         x = self.padding1(x)  # [batch_size, num_filters, seq_len, 1]
         x = self.relu(x)
-        x = self.conv(x)  # [batch_size, num_filters, seq_len-3+1, 1]
+        x = self.conv(x)  # [batch_size, num_filters, seq_len - kernel_size +1, 1]
         x = self.padding1(x)
         x = self.relu(x)
-        x = self.conv(x)  # [batch_size, num_filters, seq_len-3+1, 1]
+        x = self.conv(x)  # [batch_size, num_filters, seq_len - kernel_size + 1, 1]
 
         while x.size()[2] >= 2:
-            x = self._block(x) 
-        # 循环结束后x的形状：[batch_size, num_filters, 1, 1]
+            x = self._block(x)
+        # [batch_size, num_filters, 1, 1]
         x = x.squeeze(-1).squeeze(-1)  # [batch_size, num_filters]
         x = self.dropout(x)
         x = self.fc(x)  # [batch_size, num_classes]
@@ -86,11 +90,11 @@ if __name__ == '__main__':
                           dropout_rate, learning_rate, num_epochs, batch_size,
                           criterion_name, optimizer_name, gpu)
 
-            input = torch.LongTensor([[1, 2, 3, 4, 5], [2, 3, 4, 5, 6], [3, 4, 5, 6, 7],
-                                      [1, 3, 5, 7, 9], [2, 4, 6, 8, 10],
-                                      [1, 4, 8, 3, 6]])  # [batch_size, seq_len] = [6, 5]
-            output = model(input)
-            print(output)
+            input_data = torch.LongTensor([[1, 2, 3, 4, 5], [2, 3, 4, 5, 6], [3, 4, 5, 6, 7],
+                                           [1, 3, 5, 7, 9], [2, 4, 6, 8, 10],
+                                           [1, 4, 8, 3, 6]])  # [batch_size, seq_len] = [6, 5]
+            output_data = model(input_data)
+            print(output_data)
 
             print('The test process is done.')
         else:
@@ -99,4 +103,3 @@ if __name__ == '__main__':
         print('{} takes {} seconds'.format(args.phase, (end_time - start_time).seconds))
 
         print('Done DPCNN!')
-
