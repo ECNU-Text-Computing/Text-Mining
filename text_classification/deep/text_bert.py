dbaaa75b92e576ccd0e4545e69b808561dc84f44
unknown <selena_gu@outlook.com>
2022-08-23 22:24:50 +0800

    Update bert model

--- text_classification/deep/text_bert.py
+++ text_classification/deep/text_bert.py
@@ -4,12 +4,9 @@
 """
 BERT
 ======
-A class for something.
-======
 参考文献：
     1、https://blog.csdn.net/weixin_42237487/article/details/112355703
-    2、https://github.com/649453932/Bert-Chinese-Text-Classification-Pytorch
-注意：①需要下载谷歌中文BERT预训练模型，代码中对应修改self.bert_path的保存路径；②增加testdata.txt文件作为测试数据
+    2、https://huggingface.co/docs/transformers/v4.21.1/en/model_doc/bert#transformers.BertModel
 BERT特点：
     1、BERT可以解决词的情态表征，解决一词多义问题。
         Bert生成动态Word Embedding的思路：
@@ -18,7 +15,7 @@ BERT特点：
     2、Bert编码器（特征提取）最终可以输出两个Embedding信息：单词的Word Embedding和句子的Embedding。
     3、可以提取出Bert编码器输出的句子Embedding，经过全连接的网络层对句子进行情感判断。这个Embedding携带了当前句子的主要信息。
 模型由输入层、编码层和输出层三个部分组成
-BERT的输入：input
+BERT的输入：input_data
     1、Token Embedding：词特征（词向量）的嵌入，针对中文，目前只支持字特征嵌入
     2、Segment Embedding：词的句子级特征嵌入，针对双句子输入任务，做句子A，B嵌入，针对单句子任务，只做句子A嵌入
     3、Position Embedding：词的位置特征，针对中文，目前最大长度为 512
@@ -38,14 +35,10 @@ import datetime
 import torch
 import torch.nn as nn
 from base_model import BaseModel
-from tqdm import tqdm
 from transformers import BertModel, BertTokenizer
 
+
 # BERT文本分类
-# padding符号, bert中综合信息符号
-PAD, CLS = '[PAD]', '[CLS]'
-
-
 class BERT(BaseModel):
     def __init__(self, vocab_size, embed_dim, hidden_dim, num_classes,
                  dropout_rate, learning_rate, num_epochs, batch_size,
@@ -54,60 +47,70 @@ class BERT(BaseModel):
                                    dropout_rate, learning_rate, num_epochs, batch_size,
                                    criterion_name, optimizer_name, gpu, **kwargs)
         # 基本参数设置
-        # 每句话处理成的长度(短填长切)
-        self.pad_size = 10
-        if 'pad_size' in kwargs:
-            self.pad_size = kwargs['pad_size']
-        # 若超过1000batch效果还没提升，则提前结束训练
-        self.require_improvement = 1000
-        # BERT预训练模型存储地址：
-        self.bert_path = 'E:/GitRepo/Text_Mining/text_classification/deep/bert_pretrain'
-        # BERT切分词
-        self.tokenizer = BertTokenizer.from_pretrained(self.bert_path)
-        self.bert = BertModel.from_pretrained(self.bert_path)
+        # bert模型的输出向量为768维
+        self.hidden_dim = 768
+        # 加载的预训练模型名称
+        self.model_path = 'bert-base-uncased'
+        if 'model_path' in kwargs:
+            self.model_path = kwargs['model_path']
+        # 隐藏层维度设置
+        self.hidden_dim2 = self.hidden_dim // 2
+        if 'hidden_dim2' in kwargs:
+            self.hidden_dim2 = kwargs['hidden_dim2']
 
+        # bert模型设置
+        self.bert = BertModel.from_pretrained(self.model_path, return_dict=True, output_attentions=True,
+                                              output_hidden_states=True)
         for param in self.bert.parameters():
             param.requires_grad = True
-        
-        # 输出层
+
+        # 根据输出层的不同复杂程度，设置不同模式
+        # 默认为普通模式，仅通过一个全连接层
+        self.mode = 'normal'
         self.fc = nn.Linear(self.hidden_dim, self.num_classes)
+        if 'mode' in kwargs:
+            if kwargs['mode'] == 'adap':
+                # 进阶模式，通过两个全连接层后输出
+                print('Using adap mode.')
+                self.mode = 'adap'
+                self.fc = nn.Sequential(
+                    nn.Linear(self.hidden_dim, self.hidden_dim2),
+                    nn.Tanh(),
+                    nn.Linear(self.hidden_dim2, self.num_classes)
+                )
+            elif kwargs['mode'] == 'pro':
+                # 高级模式，通过三个全连接层后输出
+                print('Using pro mode.')
+                self.fc = nn.Sequential(
+                    nn.Linear(self.hidden_dim, int(self.hidden_dim / 2)),
+                    # nn.ReLU(),
+                    nn.Tanh(),
+                    nn.Dropout(p=self.dropout_rate),
+                    nn.Linear(int(self.hidden_dim / 2), int(self.hidden_dim / 4)),
+                    # nn.ReLU(),
+                    nn.Tanh(),
+                    nn.Dropout(p=self.dropout_rate),
+                    nn.Linear(int(self.hidden_dim / 4), self.num_classes)
+                )
+        else:
+            print('Using normal mode.')
 
-    def load_dataset(self, path):
-        pad_size = self.pad_size
-        contents = []
-        with open(path, 'r', encoding='UTF-8') as f:
-            for line in tqdm(f):
-                lin = line.strip()
-                if not lin:
-                    continue
-                content = lin
-                token = self.tokenizer.tokenize(content)
-                seq_len = len(token)
-                mask = []
-                token_ids = self.tokenizer.convert_tokens_to_ids(token)
-
-                if pad_size:
-                    if len(token) < pad_size:
-                        mask = [1] * len(token_ids) + [0] * (pad_size - len(token))
-                        token_ids += ([0] * (pad_size - len(token)))
-                    else:
-                        mask = [1] * pad_size
-                        token_ids = token_ids[:pad_size]
-                        seq_len = pad_size
-                contents.append((token_ids, seq_len, mask))
-        return contents
-
-    def forward(self, x, mask):
-        # input x: [batch_size, seq_len]
-        outputs = self.bert(x, attention_mask=mask)
-        pooled = outputs[1]
-        out = self.fc(pooled)
+    def forward(self, x):
+        # input_data x: [batch_size, seq_len]
+        output = self.bert(x)
+        if self.mode == 'adap':
+            out = output['hidden_states'][-1][:, 0]
+        else:
+            out = output['pooler_output']
+        # [batch_size, hidden_dim]
+        out = self.drop_out(out)  # [batch_size, num_classes]
+        out = self.fc(out)  # [batch_size, num_classes]
 
         # 用from pytorch_pretrained_bert时的输出
         # _, pooled = self.bert(context, attention_mask=mask, output_all_encoded_layers=False)
         # output_all_encoded_layers=True时，12层Transformer的结果全返回，存在第一个列表中，
-        # 每个encoder_output的大小为[batch_size, sequence_length, hidden_size]
-        # pool_out = [batch_size, hidden_size]，取了最后一层Transformer的输出结果的第一个单词[cls]的hidden states，其已经蕴含了整个input句子的信息了
+        # 每个encoder_output的大小为[batch_size, sequence_length, hidden_dim]
+        # pool_out = [batch_size, hidden_dim]，取了最后一层Transformer的输出结果的第一个单词[cls]的hidden states，其已经蕴含了整个input句子的信息了
         # out = self.fc(pooled)
 
         return out
@@ -122,32 +125,21 @@ if __name__ == '__main__':
 
     if args.phase == 'test':
         print('This is a test process.')
-        vocab_size, embed_dim, hidden_dim, num_classes, \
-        dropout_rate, learning_rate, num_epochs, batch_size, \
-        criterion_name, optimizer_name, gpu = 200, 64, 768, 2, 0.5, 0.0001, 3, 128, 'CrossEntropyLoss', 'Adam', 0
+        vocab_size, embed_dim, hidden_dim, num_classes, dropout_rate, learning_rate, num_epochs, batch_size, \
+        criterion_name, optimizer_name, gpu, mode \
+            = 100, 64, 32, 2, 0.5, 0.0001, 3, 3, 'CrossEntropyLoss', 'Adam', 0, 'pro'
 
-        # new an objective.
+        # 测试所用为adap模式
         model = BERT(vocab_size, embed_dim, hidden_dim, num_classes,
                      dropout_rate, learning_rate, num_epochs, batch_size,
-                     criterion_name, optimizer_name, gpu)
-        # a simple example of the input.
-        input = model.load_dataset(
-            r'E:/GitRepo/Text_Mining/text_classification/deep/bert_pretrain/testdata.txt')
-        # 应该输入文本的序列，用tokenizer变成id序列
-        m = len(input)
+                     criterion_name, optimizer_name, gpu, mode=mode)
+        # a simple example of the input_data.
+        input_data = torch.LongTensor([[1, 2, 3, 4, 5], [2, 3, 4, 5, 6], [3, 4, 5, 6, 7],
+                                  [1, 3, 5, 7, 9], [2, 4, 6, 8, 10],
+                                  [1, 4, 8, 3, 6]])  # [batch_size, seq_len] = [6, 5]
 
-        x = []  # 取第一行
-        for i in range(m):
-            x.append(input[i][0])
-        x = torch.LongTensor(x)  # x: [batch_size, seq_len] = [3, 10]
-
-        mask = []
-        for i in range(m):
-            mask.append(input[i][2])
-        mask = torch.LongTensor(mask)  # mask: 掩码，每一个句子的长度，如[5,9,7]
-
-        output = model(x, mask)
-        print(output)
+        output_data = model(input_data)
+        print(output_data)
         print('The test process is done!')
 
     else:
