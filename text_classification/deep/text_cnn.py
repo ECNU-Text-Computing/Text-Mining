6bbb58c976488317b291ad57f0694dccf05c7523
unknown <selena_gu@outlook.com>
2022-09-07 23:02:43 +0800

    Update CNN model

--- text_classification/deep/text_cnn.py
+++ text_classification/deep/text_cnn.py
@@ -2,7 +2,7 @@
 # -*- coding:utf8 -*-
 
 """
-TextCNN
+CNNLSTM
 ======
 A class for something.
 """
@@ -25,25 +25,25 @@ class TextCNN(BaseModel):
                                       criterion_name, optimizer_name, gpu, **kwargs)
 
         # 卷积核的数量
-        self.num_filters = 8
-        if 'num_filters' in kwargs:
-            self.num_filters = kwargs['num_filters']
+        self.out_channels = 8
+        if 'out_channels' in kwargs:
+            self.out_channels = kwargs['out_channels']
         # 卷积核的大小。数据类型为列表，列表长度即为卷积核数量
         self.filter_sizes = [2, 3, 4]
         if 'filter_sizes' in kwargs:
             self.filter_sizes = kwargs['filter_sizes']
         # 设置CNN模型的参数。由于可能存在多个卷积核，因此需构建多个模型，此处采用list存储不同的模型
-        self.convs = nn.ModuleList([nn.Conv2d(1, self.num_filters, (k, self.embed_dim)) for k in self.filter_sizes])
+        self.convs = nn.ModuleList([nn.Conv2d(1, self.out_channels, (k, self.embed_dim)) for k in self.filter_sizes])
         # 设置全连接层的参数
-        self.fc1 = nn.Linear(self.num_filters * len(self.filter_sizes), hidden_dim)
+        self.fc1 = nn.Linear(self.out_channels * len(self.filter_sizes), hidden_dim)
         # dropout层、输出层已在BaseModel中定义，此处直接继承
 
     # 卷积及池化过程
     def con_and_pool(self, x, conv):
         # 卷积，随后去掉额外增加的维度
-        x = F.relu(conv(x)).squeeze()  # [batch_size, num_filters, seq_len - len(filter_sizes) + 1]
+        x = F.relu(conv(x)).squeeze()  # [batch_size, out_channels, seq_len - len(filter_sizes) + 1]
         # 池化，此处采用了最大池化（Max Pooling）
-        x = F.max_pool1d(x, x.size(2)).squeeze()  # [batch_size, num_filters]
+        x = F.max_pool1d(x, x.size(2)).squeeze()  # [batch_size, out_channels]
         return x
 
     # 模型的前向传播
@@ -55,7 +55,7 @@ class TextCNN(BaseModel):
         embed = embed.unsqueeze(1)  # [batch_size, 1, seq_len, embedding]
         # 进行卷积和池化操作，并将结果按列（即维数1）拼接
         cnn_out = torch.cat([self.con_and_pool(embed, con) for con in self.convs], 1)  # size同下
-        cnn_out = self.drop_out(cnn_out)  # [batch_size, num_filters * len(filter_sizes)]
+        cnn_out = self.drop_out(cnn_out)  # [batch_size, out_channels * len(filter_sizes)]
         hidden = self.fc1(cnn_out)  # [batch_size, hidden_dim]
         out = self.fc_out(hidden)  # [batch_size, num_classes]
         return out
@@ -76,12 +76,12 @@ if __name__ == '__main__':
         # 设置模型参数
         vocab_size, embed_dim, hidden_dim, num_classes, \
         dropout_rate, learning_rate, num_epochs, batch_size, \
-        criterion_name, optimizer_name, gpu, num_filters, filter_sizes \
+        criterion_name, optimizer_name, gpu, out_channels, filter_sizes \
             = 100, 64, 32, 2, 0.5, 0.001, 3, 32, 'CrossEntropyLoss', 'Adam', 0, 2, [1, 2, 3]
         # 创建类的实例
         model = TextCNN(vocab_size, embed_dim, hidden_dim, num_classes,
                         dropout_rate, learning_rate, num_epochs, batch_size,
-                        criterion_name, optimizer_name, gpu, num_filters=num_filters, filter_sizes=filter_sizes)
+                        criterion_name, optimizer_name, gpu, out_channels=out_channels, filter_sizes=filter_sizes)
         # 传入简单数据，查看模型运行结果
         input_data = torch.LongTensor([[1, 3, 5, 7, 9], [2, 4, 6, 8, 10], [1, 4, 2, 7, 5]])
         output_data = model(input_data)
--- text_classification/deep/text_cnn_attention.py
+++ text_classification/deep/text_cnn_attention.py
@@ -16,25 +16,25 @@ class CNNAttention(TextCNN):
                                            criterion_name, optimizer_name, gpu, **kwargs)
         self.model_name = 'CNNAttention'
         # 以点积方式计算注意力
-        self.out_trans = nn.Linear(self.num_filters, self.num_filters)
-        self.w = nn.Linear(self.num_filters, 1, bias=False)
-        self.fc = nn.Linear(self.num_filters, self.num_classes)
+        self.out_trans = nn.Linear(self.out_channels, self.out_channels)
+        self.w = nn.Linear(self.out_channels, 1, bias=False)
+        self.fc = nn.Linear(self.out_channels, self.num_classes)
 
     def forward(self, x):
-        # input: [batch_size, seq_len]
+        # input_data: [batch_size, seq_len]
         out = self.embedding(x)  # [batch_size, seq_len, embed_dim]
         # 增加维度。由于卷积操作是在二维平面上进行的，而词向量内部不能拆分（拆分没有意义），因此需要增加一维
         out = out.unsqueeze(1)  # [batch_size, 1, seq_len, embed_dim]
         # 进行卷积和池化操作，并将结果按列（即维数1）拼接
-        # [batch_size, seq_len - len(filter_sizes) +1, num_filters]
+        # [batch_size, seq_len - len(filter_sizes) +1, out_channels]
         out = torch.cat([self.con_and_pool(out, conv).unsqueeze(1) for conv in self.convs], dim=1)
         # 计算注意力
-        M = F.tanh(self.out_trans(out))  # [batch_size, seq_len - len(filter_sizes) +1, num_filters]
+        M = F.tanh(self.out_trans(out))  # [batch_size, seq_len - len(filter_sizes) +1, out_channels]
         alpha = F.softmax(self.w(M), dim=1)  # [batch_size, seq_len - len(filter_sizes) +1, 1]
-        out = torch.sum(out * alpha, dim=1)  # [batch_size, num_filters]
+        out = torch.sum(out * alpha, dim=1)  # [batch_size, out_channels]
         # 上面一行也可写为：out = torch.matmul(out * alpha)
         # 通过dropout和全连接层后输出
-        out = self.drop_out(out)  # [batch_size, num_filters]
+        out = self.drop_out(out)  # [batch_size, out_channels]
         out = self.fc(out)  # [batch_size, num_classes]
         return out
 
@@ -52,12 +52,12 @@ if __name__ == '__main__':
             # 设置模型参数
             vocab_size, embed_dim, hidden_dim, num_classes, \
             dropout_rate, learning_rate, num_epochs, batch_size, \
-            criterion_name, optimizer_name, gpu, num_filters, filter_sizes \
+            criterion_name, optimizer_name, gpu, out_channels, filter_sizes \
                 = 100, 64, 32, 2, 0.5, 0.001, 3, 32, 'CrossEntropyLoss', 'Adam', 0, 2, [1, 2, 3]
             # 创建类的实例
             model = CNNAttention(vocab_size, embed_dim, hidden_dim, num_classes,
-                            dropout_rate, learning_rate, num_epochs, batch_size,
-                            criterion_name, optimizer_name, gpu, num_filters=num_filters, filter_sizes=filter_sizes)
+                                 dropout_rate, learning_rate, num_epochs, batch_size,
+                                 criterion_name, optimizer_name, gpu, out_channels=out_channels, filter_sizes=filter_sizes)
             # 传入简单数据，查看模型运行结果
             # input_data: [batch_size, seq_len] = [3, 5]
             input_data = torch.LongTensor([[1, 3, 5, 7, 9], [2, 4, 6, 8, 10], [1, 4, 2, 7, 5]])
